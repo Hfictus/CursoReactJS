bb75759fc953bb76f558e5b96add41c049e13558
Gerson Klauck <gk120hf@gmail.com>
2025-07-01 11:39:25 -0300

    ProductDetails

--- dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -4,16 +4,27 @@ import './styles.css';
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from '../../../services/product-service';
-import { useParams } from 'react-router-dom';
+//import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { ProductDTO } from '../../../models/product';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function ProductDetails() {
     
-    const params = useParams();
+    //const params = useParams();
 
-    const product = productService.findById(Number(params.productId));
-    
+    const [product, setProduct] = useState<ProductDTO>();
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/products/1")
+        .then((response) => {
+            console.log(response.data);
+            setProduct(response.data);
+        });
+    }, []);
+
+
     return(
         <main>
             <section id="product-details-section" className="dsc-container">
