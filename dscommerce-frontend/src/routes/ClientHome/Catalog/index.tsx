1b0ca4f43f0fd422a09f5c1b1391b5d67a618148
Gerson Klauck <gk120hf@gmail.com>
2025-07-03 11:01:23 -0300

    Catch request error

--- dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -3,17 +3,29 @@
 import './styles.css';
 import SearchBar from '../../../components/SearchBar';
 import ButtonNextPage from '../../../components/ButtonNextPage';
-import * as productService from '../../../services/product-service';
 import CatalogCard from '../../../components/CatalogCard';
+import { useEffect, useState } from 'react';
+import { ProductDTO } from '../../../models/product';
+import * as productService from "../../../services/product-service"
 
 export default function Catalog() {
+    
+    const [products, setProducts] = useState<ProductDTO[]>([]);
+    
+    useEffect(() => {
+            productService.findAll()
+            .then(response => {
+                setProducts(response.data.content);
+            });
+    }, []);
+
     return(
         <main>
             <section id="catalog-section" className="dsc-container">
                 <SearchBar />
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
                     {
-                        productService.findAll().map(
+                        products.map(
                             product => <CatalogCard key={product.id} product={product} />
                         )
                     }
--- dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/ProductDetails/index.tsx
@@ -4,25 +4,30 @@ import './styles.css';
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-//import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { ProductDTO } from '../../../models/product';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import * as productService from "../../../services/product-service"
 
 export default function ProductDetails() {
     
-    //const params = useParams();
+    const params = useParams();
+
+    const navigate = useNavigate();
 
     const [product, setProduct] = useState<ProductDTO>();
 
     useEffect(() => {
-        axios.get("http://localhost:8080/products/1")
-        .then((response) => {
+        productService.findById(Number(params.productId))
+        .then(response => {
             console.log(response.data);
             setProduct(response.data);
-        });
-    }, []);
+        })
+        .catch(() => {
+            navigate("/");
+        })
+    }, [params.productId, navigate]);
 
 
     return(
