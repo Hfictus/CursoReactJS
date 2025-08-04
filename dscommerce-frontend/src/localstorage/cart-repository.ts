7aca0e577831c8f55140a03515a683a990035fbb
Gerson Klauck <gk120hf@gmail.com>
2025-08-04 12:19:34 -0300

    Cart save/get

---
+++ dscommerce-frontend/src/localstorage/cart-repository.ts
@@ -0,0 +1,13 @@
+
+
+import { OrderDTO } from "../models/order";
+
+export function save(cart : OrderDTO) {
+    const str = JSON.stringify(cart);
+    localStorage.setItem("com.devsuperior.dscommerce/cart", str);
+}
+
+export function get() : OrderDTO {
+    const str = localStorage.getItem("com.devsuperior.dscommerce/cart") || '{"items"=[]}';
+    return JSON.parse(str);
+}
---
+++ dscommerce-frontend/src/services/cart-service.ts
@@ -0,0 +1,13 @@
+
+
+
+import * as cartRepository from "../localstorage/cart-repository";
+import { OrderDTO } from "../models/order";
+
+export function saveCart(cart : OrderDTO) {
+    cartRepository.save(cart);
+}
+
+export function getCart() : OrderDTO {
+    return cartRepository.get();
+}
