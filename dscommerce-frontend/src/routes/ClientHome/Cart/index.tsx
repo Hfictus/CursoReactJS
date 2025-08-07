c33dccb10df40237a15a4f6bab37e37e404fc1f5
Gerson Klauck <gk120hf@gmail.com>
2025-08-07 12:04:36 -0300

    Cart clear

--- dscommerce-frontend/src/routes/ClientHome/Cart/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Cart/index.tsx
@@ -13,6 +13,12 @@ export default function Cart() {
     
     const [cart, setCart] = useState<OrderDTO>(cartService.getCart());
     
+
+    function handleClearClick() {
+        cartService.clearCart();
+        setCart(cartService.getCart());
+    }
+
     return(
         <main>
             <section id="cart-container-section" className="dsc-container">
@@ -60,6 +66,9 @@ export default function Cart() {
                             Continuar comprando
                         </div>
                     </Link>
+                    <div onClick={handleClearClick} className="dsc-btn dsc-btn-white">
+                        Limpar carrinho
+                    </div>
                 </div>
             </section>
         </main>
