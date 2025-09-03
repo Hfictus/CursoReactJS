06bc03554cd6cb467fbfc19d7f6c545a75d84c1e
Gerson Klauck <gk120hf@gmail.com>
2025-09-03 19:46:03 -0300

    Login form, Login request

---
+++ dscommerce-frontend/src/utils/requests.ts
@@ -0,0 +1,10 @@
+
+
+
+
+import axios, { AxiosRequestConfig } from "axios";
+import { BASE_URL } from "./system";
+
+export function requestBackend(config: AxiosRequestConfig) {
+    return axios({ ...config, baseURL: BASE_URL });    
+}
--- dscommerce-frontend/src/utils/system.ts
+++ dscommerce-frontend/src/utils/system.ts
@@ -7,3 +7,7 @@
 export const BASE_URL = "http://localhost:8080";
 
 export const CART_KEY = "com.devsuperior.dscommerce/cart";
+
+export const CLIENT_ID = "myclientid";
+
+export const CLIENT_SECRET = "myclientsecret";
