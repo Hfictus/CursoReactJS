8eda8228df0f90a534858c82502e794213e3d90d
Gerson Klauck <gk120hf@gmail.com>
2025-09-05 09:45:10 -0300

    Admin page layout

--- dscommerce-frontend/src/utils/system.ts
+++ dscommerce-frontend/src/utils/system.ts
@@ -7,6 +7,7 @@
 export const BASE_URL = "http://localhost:8080";
 
 export const CART_KEY = "com.devsuperior.dscommerce/cart";
+export const TOKEN_KEY = "com.devsuperior.dscommerce/Token";
 
 export const CLIENT_ID = "myclientid";
 
