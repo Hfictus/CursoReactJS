df2153db4f95c8e08074990f61e43fe6ef2e91fb
Gerson Klauck <gk120hf@gmail.com>
2025-09-09 10:11:21 -0300

    isAuthenticated

--- dscommerce-frontend/src/models/auth.ts
+++ dscommerce-frontend/src/models/auth.ts
@@ -2,7 +2,20 @@
 
 
 
+
+
+export type RoleEnum = "ROLE_ADMIN" | "ROLE_CLIENT";
+
 export type CredentialsDTO = {
     username: string;
     password: string;
 }
+
+//incluí nbf abaixo *******
+export type AccessTokenPayloadDTO = {
+    nbf: number;
+    exp: number;
+    username: string;
+    authorities: RoleEnum[];
+}
+//o professor usou user_name. No meu PAYLOAD aparece username
