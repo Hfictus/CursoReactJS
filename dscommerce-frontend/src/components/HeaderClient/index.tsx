9c4f3d15fac2a07e102219d956026d913a2ac710
Gerson Klauck <gk120hf@gmail.com>
2025-09-13 09:58:09 -0300

    Context token payload

--- dscommerce-frontend/src/components/HeaderClient/index.tsx
+++ dscommerce-frontend/src/components/HeaderClient/index.tsx
@@ -5,9 +5,16 @@
 import './styles.css';
 import { Link } from 'react-router-dom';
 import CartIcon from '../CartIcon';
+import iconAdmin from "../../assets/admin.svg";
+import * as authService from "../../services/auth-service";
+import { ContextToken } from '../../utils/context-token';
+import { useContext } from 'react';
 
 export default function HeaderClient() {
-    return(
+  
+  const { contextTokenPayload } = useContext(ContextToken);
+  
+  return(
       <header className="dsc-header-client">
         <nav className="dsc-container">
           <Link to="/">
@@ -15,6 +22,15 @@ export default function HeaderClient() {
           </Link>
           <div className="dsc-navbar-right">
             <div className="dsc-menu-items-container">
+              {
+                contextTokenPayload &&
+                authService.hasAnyRoles(["ROLE_ADMIN"]) &&
+                <Link to="/admin">
+                  <div className="dsc-menu-item">
+                    <img src={iconAdmin} alt="Admin" />
+                  </div>
+                </Link>
+              }
               <Link to="/cart">
                 <div className="dsc-menu-item">
                   <CartIcon />
