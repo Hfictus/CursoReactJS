16057b4c1386c6f1676befe793ad4c6701340b89
Gerson Klauck <gk120hf@gmail.com>
2025-09-24 10:25:28 -0300

    Admin area routes

--- dscommerce-frontend/src/components/HeaderAdmin/index.tsx
+++ dscommerce-frontend/src/components/HeaderAdmin/index.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import homeIcon from "../../assets/home-svgrepo-com 1.svg";
 import productsIcon from "../../assets/Group 20.svg";
 import LoggedUser from "../LoggedUser";
+import { NavLink } from "react-router-dom";
 
 export default function HeaderAdmin() {
     return(
@@ -13,14 +14,24 @@ export default function HeaderAdmin() {
                 <h1>DSC Admin</h1>
                 <div className="dsc-navbar-right">
                     <div className="dsc-menu-items-container">
-                        <div className="dsc-menu-item">
-                            <img src={homeIcon} alt="Início" />
-                            <p>Início</p>
-                        </div>
-                        <div className="dsc-menu-item">
-                            <img src={productsIcon} alt="Cadastro de produtos" />
-                            <p className="dsc-menu-item-active">Produtos</p>
-                        </div>
+                        <NavLink
+                            to="/admin/home"
+                            className={({isActive}) => isActive ? "dsc-menu-item-active": ""}
+                        >
+                            <div className="dsc-menu-item">
+                                <img src={homeIcon} alt="Início" />
+                                <p>Início</p>
+                            </div>
+                        </NavLink>
+                        <NavLink
+                            to="/admin/products"
+                            className={({isActive}) => isActive ? "dsc-menu-item-active" : ""}
+                        >
+                            <div className="dsc-menu-item">
+                                <img src={productsIcon} alt="Cadastro de produtos" />
+                                <p>Produtos</p>
+                            </div>
+                        </NavLink>
                     </div>
                     <LoggedUser />
                 </div>
